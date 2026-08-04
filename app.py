2d5df7331804b0a2e85d9723d31ef536448b745f
David Odhiambo <davidodhis467@gmail.com>
2026-08-05 01:55:24 +0300

    Add home and sitemap routes to Flask app

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from flask import send_from_directory
+from flask import Flask, render_template, send_from_directory
 
-@app.route('/robots.txt')
-def robots():
-    return send_from_directory('static', 'robots.txt')
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
-
 
 @app.route('/skills')
 def skills():
     return render_template('skills.html')
 
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('static', 'robots.txt')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'sitemap.xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
